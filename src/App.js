dd2531d514e860f94523081c644c0155d4aaf9e2
AbdullahRajpoot404 <abdullaharshad404@gmail.com>
2024-01-04 04:13:18 -0800

    Abdullah Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./MyComponents/Navbar";
+import Navbar from "./mainComponets/Navbar";
 
 function App() {
   return (
--- src/mainComponets/Navbar.js
+++ src/mainComponets/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <>
       <div className="navbarImg">
-        <nav className="navbar navbar-expand-lg">
+        <nav className="navbar navbar-expand-lg navbar-dark">
           <div className="container-fluid">
             <a className="navbar-brand" to="#">
               Navbar
@@ -43,15 +43,7 @@ export default function Navbar() {
                 </li>
               </ul>
               <form className="d-flex" role="search">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline-success" type="submit">
-                  Search
-                </button>
+                <button></button>
               </form>
             </div>
           </div>
