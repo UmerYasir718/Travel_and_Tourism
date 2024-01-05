3084337bcca054c685f0f81083858c8b4bf58014
AbdullahRajpoot404 <abdullaharshad404@gmail.com>
2024-01-05 11:22:49 -0800

    Abdullah Footer Update

--- src/mainComponets/LowerFooter.js
+++ src/mainComponets/LowerFooter.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function LowerFooter() {
   return (
     <>
-      <div className="container">
+      <div className="container ">
         <div className="row my-5 text-secondary">
-          <div className="col-3 ">
+          <div className="col-lg-3 col-md-6 col-sm-12">
             <h5>
               Got Question? Call us 24/7 <br />
               Call us: <b>0301-1234567</b>
@@ -22,9 +22,51 @@ export default function LowerFooter() {
               </a>
             </div>
           </div>
-          <div className="col-3"></div>
-          <div className="col-3"></div>
-          <div className="col-3"></div>
+          <div className="col-lg-3 headerSection2 col-md-6 col-sm-12">
+            <h4>Products</h4>
+            <ul>
+              <li>
+                <a href="">Flights</a>
+              </li>
+              <li>
+                <a href="">Visas</a>
+              </li>
+              <li>
+                <a href="">Rewards</a>
+              </li>
+              <li>
+                <a href="">Site Map</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 headerSection2 col-md-6 col-sm-12">
+            <h4>Company</h4>
+            <ul>
+              <li>
+                <a href="">Our Stories</a>
+              </li>
+              <li>
+                <a href="">Terms & Conditions</a>
+              </li>
+              <li>
+                <a href="">Careers</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 headerSection2 col-md-6 col-sm-12">
+            <h4>Support</h4>
+            <ul>
+              <li>
+                <a href="">My Accounts</a>
+              </li>
+              <li>
+                <a href="">Contact Us</a>
+              </li>
+              <li>
+                <a href="">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
