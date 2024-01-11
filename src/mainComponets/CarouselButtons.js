edd0ffa83f23cf6747a1317c2118bb53bd0ecd81
UmerYasir718 <you@example.com>
2024-01-12 02:27:08 +0500

    Umer

--- src/mainComponets/CarouselButtons.js
+++ src/mainComponets/CarouselButtons.js
@@ -4,24 +4,25 @@ import { Link } from "react-router-dom";
 export default function CarouselButtons() {
   return (
     <>
-      <div className="container">
+      <div className="container carouselBtnRow">
+        <h1>Best Deals</h1>
         <Link>
-          <button type="button" class="btn carouselBtn">
+          <button type="button" className="btn carouselBtn">
             Visas
           </button>
         </Link>
         <Link>
-          <button type="button" class="btn carouselBtn">
+          <button type="button" className="btn carouselBtn">
             Flights
           </button>
         </Link>
         <Link>
-          <button type="button" class="btn carouselBtn">
+          <button type="button" className="btn carouselBtn">
             Bank Offers
           </button>
         </Link>
         <Link>
-          <button type="button" class="btn carouselBtn">
+          <button type="button" className="btn carouselBtn">
             Rewards
           </button>
         </Link>
