74574ae6c347ec6a4e8c540f694400f39eb87c79
UmerYasir718 <umeryasir718@gmail.com>
2024-01-12 03:33:42 +0500

    UmerYasir718

---
+++ src/MyComponents/Visa/Visa.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../mainComponets/Navbar";
+export default function Visa() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="alert alert-danger " style={{ marginTop: "100px" }}>
+        Page Is Under Development
+      </h1>
+    </div>
+  );
+}
