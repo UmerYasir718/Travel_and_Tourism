e000b6a5d009e94574347507264a236c411fbd4b
AbdullahRajpoot404 <abdullaharshad404@gmail.com>
2024-01-13 07:08:53 -0800

    Abdllah

--- src/MyComponents/Country/CountryContent.js
+++ src/MyComponents/Country/CountryContent.js
@@ -29,7 +29,60 @@ export default function CountryContent() {
               <b className="text-primary"> 1-NADRA </b>
               <h5>Documents Required for Australia Visa from Pakistan</h5>
               Below are the requirements for an Australia visa from Pakistan
-              <h5 className="my-4">Pasport:</h5>
+              <div className="pasport">
+                <h5 className="my-4">Pasport:</h5>
+                <ul>
+                  <li>Original Passport as well as all old passports</li>
+                  <li>
+                    Please note passport must fulfill all the below conditions
+                  </li>
+                  <li>
+                    The passport should have a minimum of 2 blank pages & should
+                    be valid for a period of 6 months validity at the time of
+                    travel
+                  </li>
+                </ul>
+              </div>
+              <div className="requireDocument">
+                <h5 className="my-4">
+                  Required Documents for Australia Visa from Pakistan
+                </h5>
+                <ul>
+                  <li>
+                    National ID card scanned colored copy. Both the back and
+                    front side
+                  </li>
+                  <li>Family Registration Certificate from NADRA (FRC)</li>
+                  <li>Marriage Registration Certificate from NADRA (MRC)</li>
+                  <li>
+                    A certified copy of your National Tax Number (NTN)
+                    Registration Certificate issued by the Federal Board of
+                    Revenue.
+                  </li>
+                  <li>
+                    The tax returns from the most recent fiscal year (Last Year)
+                    for your Australian visa application.
+                  </li>
+                  <li>Relative in Australia (if any)</li>
+                  <li>Any refusal from Australia or any other country</li>
+                </ul>
+              </div>
+            </p>
+            <p>
+              <b className="text-primary">2- Photographs:</b>
+              <h5>Photographs for Australia Visa from Pakistan</h5>
+              <ul>
+                <li>
+                  Three recent colored photographs of size 2 x 2 Inches white
+                  background with matt or semi-matt finish with 80% face
+                  coverage, white background, and without border. (Photograph
+                  should not be more than 3 months old, scanned/stapled, and
+                  should not be used in any of the previous visas)
+                </li>
+              </ul>
+            </p>
+            <p>
+              <b className="text-primary">3- Additional Documents</b>
             </p>
           </div>
         </div>
