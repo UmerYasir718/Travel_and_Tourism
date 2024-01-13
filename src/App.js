d34f26a6db1240a9c2e0713a4701e57317d29067
MuhammadNauman1960 <muhammadnauman1960@gmail.com>
2024-01-13 06:21:03 -0800

    Nauman

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import MainRoutes from "./mainComponets/MainRoutes";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
