ee004b6323360897faba900b07a1e55e0293e799
AbdullahRajpoot404 <abdullaharshad404@gmail.com>
2024-01-13 06:49:02 -0800

    Abdullah

---
+++ src/MyComponents/Country/CountryContent.js
@@ -0,0 +1,39 @@
+import React from "react";
+export default function CountryContent() {
+  return (
+    <>
+      <div className="container">
+        <h2>Australia</h2>
+        <p>
+          <ul>
+            <li>
+              <b>Visa Type:</b>Visit Visa
+            </li>
+            <li>
+              <b>Entry Type:</b>Single
+            </li>
+            <li>
+              <b>Processing Time:</b> 4-8 Weeks
+            </li>
+          </ul>
+          Australia visa from Pakistan is easy to acquire with{" "}
+          <b className="text-danger">Platform Intl</b>. Australian visitor visa
+          from Pakistan takes a maximum of Four to Eight weeks to process.
+          Platform Intl also facilitates Australia visas.
+        </p>
+        {/* ---------------------------------------------------Cards section----------------------------------------------------------------- */}
+        <div class="card">
+          <h3 class="card-header ">Document Required </h3>
+          <div class="card-body">
+            <p>
+              <b className="text-primary"> 1-NADRA </b>
+              <h5>Documents Required for Australia Visa from Pakistan</h5>
+              Below are the requirements for an Australia visa from Pakistan
+              <h5 className="my-4">Pasport:</h5>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
