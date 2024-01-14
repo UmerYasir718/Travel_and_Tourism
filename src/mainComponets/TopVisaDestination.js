7077314e91102b273f68cb6d87d3c29458c51402
UmerYasir718 <you@example.com>
2024-01-14 19:56:56 +0500

    Second

--- src/mainComponets/TopVisaDestination.js
+++ src/mainComponets/TopVisaDestination.js
@@ -1,82 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function TopVisaDestination() {
+  const [countryData, setCountryData] = useState("");
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://travelandtourismapis-production.up.railway.app/home/countries"
+      );
+      const json = await response.json();
+      setCountryData(json);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      alert(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="container topVisaDestination mb-3">
       <div className="row">
         <h2 className="fw-bold mb-4">Top Visa Destination</h2>
-        <div className="col-md-6 mb-3">
-          <div className="smallCard">
-            <div className="smallCardTitle">
-              <div className="title">
-                Pakistan
-                <br />
-                <span className="smallCardPrice">PKR_12000</span>
+
+        {Array.isArray(countryData) ? (
+          countryData.map((country) => (
+            <div className="col-md-6 mb-3">
+              <div className="smallCard">
+                <div className="smallCardTitle">
+                  <div
+                    className="title"
+                    key={country._id}
+                    value={country.countryName}
+                  >
+                    {country.countryName}
+                    <br />
+                    <span className="smallCardPrice">
+                      PKR_{country.countryPrice}
+                    </span>
+                  </div>
+                </div>
+                <div className="CardLink">&gt;</div>
               </div>
             </div>
-            <div className="CardLink">&gt;</div>
-          </div>
-        </div>
-        <div className="col-md-6 mb-3">
-          <div className="smallCard">
-            <div className="smallCardTitle">
-              <div className="title">
-                Pakistan
-                <br />
-                <span className="smallCardPrice">PKR_12000</span>
-              </div>
-            </div>
-            <div className="CardLink">&gt;</div>
-          </div>
-        </div>
-        <div className="col-md-6 mb-3">
-          <div className="smallCard">
-            <div className="smallCardTitle">
-              <div className="title">
-                Pakistan
-                <br />
-                <span className="smallCardPrice">PKR_12000</span>
-              </div>
-            </div>
-            <div className="CardLink">&gt;</div>
-          </div>
-        </div>
-        <div className="col-md-6 mb-3">
-          <div className="smallCard">
-            <div className="smallCardTitle">
-              <div className="title">
-                Pakistan
-                <br />
-                <span className="smallCardPrice">PKR_12000</span>
-              </div>
-            </div>
-            <div className="CardLink">&gt;</div>
-          </div>
-        </div>
-        <div className="col-md-6 mb-3">
-          <div className="smallCard">
-            <div className="smallCardTitle">
-              <div className="title">
-                Pakistan
-                <br />
-                <span className="smallCardPrice">PKR_12000</span>
-              </div>
-            </div>
-            <div className="CardLink">&gt;</div>
-          </div>
-        </div>
-        <div className="col-md-6 mb-3">
-          <div className="smallCard">
-            <div className="smallCardTitle">
-              <div className="title">
-                Pakistan
-                <br />
-                <span className="smallCardPrice">PKR_12000</span>
-              </div>
-            </div>
-            <div className="CardLink">&gt;</div>
-          </div>
-        </div>
+          ))
+        ) : (
+          <option key="noCountry" disabled>
+            No Country Found
+          </option>
+        )}
       </div>
     </div>
   );
