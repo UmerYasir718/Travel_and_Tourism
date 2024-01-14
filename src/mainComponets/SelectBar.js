506f54f9d2e0b36b619c5f207ebd8fcbbe5d57bc
UmerYasir718 <you@example.com>
2024-01-14 19:48:21 +0500

    First Integration

--- src/mainComponets/SelectBar.js
+++ src/mainComponets/SelectBar.js
@@ -1,27 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function SelectBar() {
-    return (
-        <div>
-            <div className="row SelectSetting">
-                <div className="col- col-sm-12 col-md-6 col-lg-6 SelectBar">
-                    <select
-                        className="form-select form-select-lg "
-                        aria-label="Large select example"
-                    >
-                        <option selected>Select Country</option>
-                        <option value="1">Pakistan</option>
-                        <option value="2">Dubai</option>
-                        <option value="3">Canada</option>
-                        <option value="1">Pakistan</option>
-                        <option value="2">Dubai</option>
-                        <option value="3">Canada</option>
-                    </select>
-                </div>
-                <div className="col- col-sm-12 col-md-6 col-lg-6 SelectBtn">
-                    <button type="button" className="btn btn-primary btn-lg">Search</button>
-                </div>
-            </div>
+  const [countryData, setCountryData] = useState("");
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://travelandtourismapis-production.up.railway.app/home/countries"
+      );
+      const json = await response.json();
+      setCountryData(json);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      alert(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <div className="row SelectSetting">
+        <div className="col- col-sm-12 col-md-6 col-lg-6 SelectBar">
+          <select
+            className="form-select form-select-lg "
+            aria-label="Large select example"
+          >
+            <option key="default" defaultValue>
+              Select Country
+            </option>
+            {Array.isArray(countryData) ? (
+              countryData.map((country) => (
+                <option key={country._id} value={country.countryName}>
+                  {country.countryName}
+                </option>
+              ))
+            ) : (
+              <option key="noCountry" disabled>
+                No Country Found
+              </option>
+            )}
+          </select>
         </div>
-    )
+        <div className="col- col-sm-12 col-md-6 col-lg-6 SelectBtn">
+          <button type="button" className="btn btn-primary btn-lg">
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
