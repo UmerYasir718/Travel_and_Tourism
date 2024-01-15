ed6115ca98c9c7fa9cfa166a0f5eb727bd0a3ca6
UmerYasir718 <you@example.com>
2024-01-15 13:31:08 +0500

    Umer

--- src/mainComponets/CarouselOfPapularRoutes.js
+++ src/mainComponets/CarouselOfPapularRoutes.js
@@ -7,19 +7,19 @@ export default function CarouselOfPapularRoutes() {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 1024 },
+      breakpoint: { max: 4000, min: 1198 },
       items: 4,
     },
     desktop: {
-      breakpoint: { max: 1024, min: 800 },
+      breakpoint: { max: 1198, min: 992 },
       items: 3,
     },
     tablet: {
-      breakpoint: { max: 800, min: 464 },
+      breakpoint: { max: 992, min: 650 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 650, min: 0 },
       items: 1,
     },
   };
@@ -32,7 +32,12 @@ export default function CarouselOfPapularRoutes() {
           infinite={true}
           autoPlay={true}
           autoPlaySpeed={3000}
-          removeArrowOnDeviceType={["tablet", "mobile"]}
+          removeArrowOnDeviceType={[
+            "tablet",
+            "mobile",
+            "desktop",
+            "superLargeDesktop",
+          ]}
         >
           <MostPopularRoutes />
           <MostPopularRoutes />
--- src/mainComponets/MostPopularRoutes.js
+++ src/mainComponets/MostPopularRoutes.js
@@ -3,7 +3,30 @@ import React from "react";
 export default function MostPopularRoutes() {
   return (
     <>
-      <div className="card" style={{ width: "18rem" }}>
+      <div className="col-12 col-sm-6 col-md-3 col-lg-3 mb-3 MPRCardSetting">
+        <div className="float-start" style={{ width: "25rem" }}>
+          <figure className="position-relative">
+            <img
+              src="https://www.yugo.pk/routes/1645006505.Lahore%20to%20Dubai%20Flights.png"
+              alt=""
+              className="card-img-top MPRCard-img"
+            />
+          </figure>
+          <div className="">
+            <div className="card-detail d-flex justify-content-center align-content-center align-items-center">
+              <div className="card-detail-1">
+                <span className="Destination">Lahore To Dubai</span>
+                <br />
+                <span className="MPRStartingFrom">Starting from</span>
+
+                <div className="MPRPrice"> PKR 12,000</div>
+              </div>
+            </div>
+          </div>
+        </div>
+                  
+      </div>
+      {/* <div className="cardSetting" style={{ width: "18rem" }}>
         <img
           src="https://www.yugo.pk/routes/1645006505.Lahore%20to%20Dubai%20Flights.png"
           className="card-img-top my-4"
@@ -12,12 +35,12 @@ export default function MostPopularRoutes() {
         <div className="card-body">
           <h5 className="card-title text-center">Karachi ➖ Islamabad</h5>
           <p className="text-center">
-            <b className="text-primary">starting From</b>
+            <b className="text-primary">Starting From</b>
             <br />
             <b>PKR:12,000</b>
           </p>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
