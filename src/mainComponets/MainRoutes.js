d7198f3d0dd19e5628e328685626efff6f70aae8
AbdullahRajpoot404 <abdullaharshad404@gmail.com>
2024-01-15 03:31:26 -0800

    Abdullah

--- src/mainComponets/MainRoutes.js
+++ src/mainComponets/MainRoutes.js
@@ -6,7 +6,8 @@ import Home from "../MyComponents/HomePage/Home";
 import Offers from "../MyComponents/Offers/Offers";
 import Packages from "../MyComponents/Packages/Packages";
 import Visa from "../MyComponents/Visa/Visa";
-import Login from "./Login";
+// import Login from "./Login";
+import SignUp from "./SignUp";
 export default function MainRoutes() {
   return (
     <div>
@@ -18,7 +19,7 @@ export default function MainRoutes() {
           <Route exact path="/offers" element={<Offers />}></Route>
           <Route exact path="/packages" element={<Packages />}></Route>
           <Route exact path="/country" element={<Country />}></Route>
-          <Route exact path="/userAccount" element={<Login />}></Route>
+          <Route exact path="/userAccount" element={<SignUp />}></Route>
         </Routes>
       </Router>
     </div>
--- src/mainComponets/SignUp.js
+++ src/mainComponets/SignUp.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Navbar from "./Navbar";
 
-export default function Login() {
+export default function SignUp() {
   return (
     <>
-      <Navbar />
       <div className="bg-dark">
-        <div className=" wrapper container d-flex justify-content-center align-items-center w-100 ">
-          <div className="login">
-            <h4> Login </h4>
+        <div className="container wrapper d-flex justify-content-center align-items-center w-100">
+          <div className="signUp">
+            <h4>SignUp</h4>
             <hr />
             <form className="need-validation">
               <div className="mb-3 was-validated">
@@ -17,14 +15,11 @@ export default function Login() {
                 </label>
                 <input
                   type="email"
-                  className="form-control "
+                  className="form-control"
                   required
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
                 />
-                <div className="invalid-feedback">
-                  <p>Make sure you enterd valid email</p>
-                </div>
               </div>
               <div className="mb-3 was-validated">
                 <label for="exampleInputPassword1" className="form-label">
@@ -33,30 +28,23 @@ export default function Login() {
                 <input
                   type="password"
                   className="form-control"
+                  required
                   id="exampleInputPassword1"
-                  required
                 />
-                <div className="invalid-feedback">
-                  <p>Remember your password and dont share it with others</p>
-                </div>
               </div>
-              <div className="mb-3 form-check was-validated">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                  required
-                />
-                <label
-                  className="form-check-label"
-                  for="exampleCheck1"
-                  required
-                >
-                  Check me out
+              <div className="mb-3 my-3 was-validated">
+                <label for="exampleInputPassword1" className="form-label">
+                  Confirm Password
                 </label>
+                <input
+                  type="password"
+                  required
+                  className="form-control"
+                  id="exampleInputPassword1"
+                />
               </div>
               <button type="submit" className="btn btn-primary w-100">
-                Login
+                SignUp
               </button>
             </form>
           </div>
