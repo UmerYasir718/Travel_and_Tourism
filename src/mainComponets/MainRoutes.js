51741d1a5dec9ddd400ffeabeee51c1c3bb3dbcb
UmerYasir718 <you@example.com>
2024-01-15 20:40:26 +0500

    Umer

--- src/mainComponets/MainRoutes.js
+++ src/mainComponets/MainRoutes.js
@@ -6,19 +6,13 @@ import Home from "../MyComponents/HomePage/Home";
 import Offers from "../MyComponents/Offers/Offers";
 import Packages from "../MyComponents/Packages/Packages";
 import Visa from "../MyComponents/Visa/Visa";
-// import Login from "./Login";
 import Login from "./Login";
 import SignUp from "./SignUp";
-<<<<<<< HEAD
 import StateContext from "./StateContext";
-=======
-import FreeConsultancy from "./FreeConsultancy";
->>>>>>> 853e48027610b031d26024c3bb683ff6197049fe
 export default function MainRoutes() {
   const [user, setUser] = useState([]);
   return (
     <div>
-<<<<<<< HEAD
       <StateContext.Provider value={{ user, setUser }}>
         <Router basename="/">
           <Routes>
@@ -33,19 +27,6 @@ export default function MainRoutes() {
           </Routes>
         </Router>
       </StateContext.Provider>
-=======
-      <Router basename="/">
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/visa" element={<Visa />}></Route>
-          <Route exact path="/flight" element={<Flight />}></Route>
-          <Route exact path="/offers" element={<Offers />}></Route>
-          <Route exact path="/packages" element={<Packages />}></Route>
-          <Route exact path="/country" element={<Country />}></Route>
-          <Route exact path="/userAccount" element={<SignUp />}></Route>
-        </Routes>
-      </Router>
->>>>>>> 853e48027610b031d26024c3bb683ff6197049fe
     </div>
   );
 }
