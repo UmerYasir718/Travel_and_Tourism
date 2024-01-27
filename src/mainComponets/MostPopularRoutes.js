794365d6f737f66b55410b096e1d35cfcd0e4da7
UmerYasir718 <umeryasir718@gmail.com>
2024-01-28 01:16:30 +0500

    Umer

--- src/mainComponets/MostPopularRoutes.js
+++ src/mainComponets/MostPopularRoutes.js
@@ -43,57 +43,61 @@ export default function MostPopularRoutes() {
       {/* <div className="row"> */}
       <div className="container mb-3">
         <h2 className="fw-bold mb-4">Most Popular Routes</h2>
-        <Carousel
-          responsive={responsive}
-          infinite={true}
-          autoPlay={true}
-          autoPlaySpeed={3000}
-          removeArrowOnDeviceType={[
-            "tablet",
-            "mobile",
-            "desktop",
-            "superLargeDesktop",
-          ]}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignContent: "center",
-          }}
-        >
-          {Array.isArray(routesData) ? (
-            routesData.map((routes, index) => (
-              <div className="col-12 col-sm-6 col-md-3 col-lg-3  MPRCardSetting gap-3">
-                <div className="float-start" style={{ width: "25rem" }}>
-                  <figure className="position-relative">
-                    <img
-                      src={routes.image}
-                      alt=""
-                      className="card-img-top MPRCard-img d-flex justify-content-center align-content-center align-items-center"
-                    />
-                  </figure>
-                  <div className="">
-                    <div className="card-detail d-flex justify-content-center align-content-center align-items-center">
-                      <div className="card-detail-1">
-                        <span className="Destination">
-                          {routes.srcPlace} To {routes.desPlace}
-                        </span>
-                        <br />
-                        <span className="MPRStartingFrom">Starting from</span>
+        <div className="container">
 
-                        <div className="MPRPrice">PKR {routes.price}</div>
+
+          <Carousel
+            responsive={responsive}
+            infinite={true}
+            autoPlay={true}
+            autoPlaySpeed={3000}
+            removeArrowOnDeviceType={[
+              "tablet",
+              "mobile",
+              "desktop",
+              "superLargeDesktop",
+            ]}
+          // style={{
+          //   display: "flex",
+          //   justifyContent: "center",
+          //   alignContent: "center",
+          // }}
+          >
+            {Array.isArray(routesData) ? (
+              routesData.map((routes, index) => (
+                <div className="col-12 col-sm-6 col-md-3 col-lg-3  MPRCardSetting gap-3 ">
+                  <div className="float-start  container" style={{ width: "28rem" }}>
+                    <figure className="position-relative">
+                      <img
+                        src={routes.image}
+                        alt=""
+                        className="card-img-top MPRCard-img d-flex justify-content-center align-content-center align-items-center"
+                      />
+                    </figure>
+                    <div className="container d-flex justify-content-center align-content-center align-items-center">
+                      <div className="card-detail ">
+                        <div className="card-detail-1">
+                          <span className="Destination">
+                            {routes.srcPlace} To {routes.desPlace}
+                          </span>
+                          <br />
+                          <span className="MPRStartingFrom">Starting from</span>
+
+                          <div className="MPRPrice">PKR {routes.price}</div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))
-          ) : (
-            <ul>
-              <li className="CountryName list-group-item">No Data Available</li>
-            </ul>
-          )}
-          {/* </div> */}
-        </Carousel>
+              ))
+            ) : (
+              <ul>
+                <li className="CountryName list-group-item">No Data Available</li>
+              </ul>
+            )}
+            {/* </div> */}
+          </Carousel>
+        </div>
       </div>
     </>
   );
